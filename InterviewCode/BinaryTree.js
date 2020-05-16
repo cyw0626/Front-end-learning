cbbd0a01c6f5f677ca3de9b6b56c37911e098e25
Yuwang Cui <44495079+cyw0626@users.noreply.github.com>
2020-05-16 11:10:58 +0800

    Create BinaryTree.js

---
+++ InterviewCode/BinaryTree.js
@@ -0,0 +1,37 @@
+//求取二叉树的深度    
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function(root) {
+    //DFS
+    if(root==null) return 0;
+    let left=maxDepth(root.left);
+    let right=maxDepth(root.right);
+    return Math.max(left,right)+1;
+    //BFS
+    let level=0;
+    if(root==null) return level;
+    let queue=[root];
+    while(queue.length){
+        let len=queue.length;
+        while(len--){
+            let cur=queue.pop();
+            if(cur.left){
+                queue.unshift(cur.left);
+            }
+            if(cur.right){
+                queue.unshift(cur.right);
+            }
+        }
+        level+=1;
+    }
+    return level;
+};
