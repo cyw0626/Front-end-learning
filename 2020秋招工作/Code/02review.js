0cd46f7010259254ea37652cf26780031bd47350
cyw0626 <1916534795@qq.com>
2020-09-15 20:43:20 +0800

    20200915

---
+++ 2020秋招工作/Code/02review.js
@@ -0,0 +1,82 @@
+//1.
+//用setTimeout实现setInterval
+function myInterval(fn,time){
+    function interval(){
+        setTimeout(interval,time);
+        fn();
+    }
+    interval();
+}
+//2.
+//深拷贝 
+//缺点：
+//对象里有函数，无法拷贝；无法拷贝原型链上的属性；数据层次很深，容易栈溢出      
+var objClone = JSON.parse(JSON.stringify(obj));
+// 无法保持引用，容易栈溢出   
+function deepClone(obj){
+    if(obj && typeof obj ==="object"){
+        var objClone=Array.isArray(obj)?[]:{};
+    }
+    for(var key in obj){
+        if(obj.hasOwnProperty(key)){
+            if(obj[key] && typeof obj[key] === 'object'){
+                objClone[key] = deepClone(obj[key]);
+            }else{
+                objClone[key] = obj[key];
+            }
+        }
+    }
+    return objClone;
+}
+//防止栈溢出函数
+function deepClone(obj){
+    var root = {};
+    var loopList = [
+        {
+            parent:root,
+            key:undefined,
+            data:obj
+        }
+    ];
+    while(loopList.length){
+        var node = loopList.pop();
+        var parent = node.parent;
+        var key = node.key;
+        var data = node.data;
+        var res = parent;
+        if(typeof key !== 'undefined'){
+            res = parent[key]={}
+        }
+        for(var k in data){
+            if(data.hasOwnProperty(k)){
+                if(typeof data[k] === 'object'){
+                    loopList.push({
+                        parent:res,
+                        key:k,
+                        data:data[k]
+                    })                    
+                }else{
+                    res[k] = data[k]
+                }
+            }
+        }
+    }
+    return root;
+}
+//3.
+//下拉框
+<a href='javascript:void(0);' onclick ='jsButton()'/>
+//4.
+//取消冒泡  
+function stopBubble(e){
+    if(e && e.stopPropagation){
+        e.stopPropagation();
+    }else{
+        window.event.cancelBubble = true;
+    }
+}
+//5.性能优化方法  
+//雅虎军规
+//减少DOM操作
+//减少HTTP请求
+//
